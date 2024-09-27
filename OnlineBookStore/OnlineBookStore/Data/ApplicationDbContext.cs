c0e11cfb82efd6f1d414c7a0f314bd76e5c4208f
Md Raufur Rahim <raufurrahim2000@gmail.com>
2024-09-27 19:27:04 +0600

    Category List Page Has been Created.

---
+++ OnlineBookStore/OnlineBookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookStore.Models;
+
+namespace OnlineBookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Dot Net", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Physics", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Math", DisplayOrder = 3 }
+                );
+        }
+    }
+}

0513966e1900a1e5d5665065f6ab472987e086d2
Md Raufur Rahim <raufurrahim2000@gmail.com>
2024-10-02 00:08:19 +0600

    Create Button is added with its server and client side configuration. Edit and Delete controllers are added without views.

--- OnlineBookStore/OnlineBookStore/Controllers/CategoryController.cs
+++ OnlineBookStore/OnlineBookStore/Controllers/CategoryController.cs
@@ -16,5 +16,36 @@ namespace OnlineBookStore.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name Can not match the Order Number");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            //InCaseOf Different Controller return RedirectToAction("Action","Controller")
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id) {
+            if (id == null || id == 0) { 
+             return NotFound();
+            }
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj) { 
+        return View(obj);
+        }
     }
 }
